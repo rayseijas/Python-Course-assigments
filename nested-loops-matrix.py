109e29870910013ff5a0f7eb8fbe1de3cf529437
rayseijas <rayseijas856@gmail.com>
2026-06-16 21:29:50 -0400

    Create nested-loops-matrix.py

---
+++ nested-loops-matrix.py
@@ -0,0 +1,13 @@
+rows = int(input("Enter the number of rows: "))
+colums = int(input("Enter the number of columns: "))
+symbol = input("Enter the symbol: ")
+colums = colums
+
+for x in range(rows):
+
+    for y in range(colums):
+        print(symbol, end="")
+    print()
+
+
+
